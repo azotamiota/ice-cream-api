75cf64dd9cb670d70cb6d91c35662d2923ab0173
azotamiota <azotamiota@gmail.com>
2022-07-15 12:43:47 +0100

    create database

---
+++ server/data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+        id: 1,
+        flavour: 'pistachio',
+        vegan: true
+    },
+    {
+        id: 2,
+        flavour: 'salted caramel',
+        vegan: true
+    },
+    {
+        id: 3,
+        flavour: 'lemon sorbet',
+        vegan: false
+    },
+    {
+        id: 4,
+        flavour: 'vanilla',
+        vegan: false
+    },
+    {
+        id: 5,
+        flavour: 'mint',
+        vegan: true
+    }
+]
+
+module.exports = data
