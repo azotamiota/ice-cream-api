c3e71b3efd32957526f20e587aff18b4a053756e
azotamiota <azotamiota@gmail.com>
2022-07-15 12:44:19 +0100

    update test (still failing)

--- server/test/app.test.js
+++ server/test/app.test.js
@@ -9,12 +9,31 @@ describe("API", () => {
         api = app.listen(3030)
     })
 
-    afterAll(() => {
-        api.close();
+    afterAll((done) => {
+        api.close(done);
     })
 
-    it("Responds to GET request at  with a 200 status", () => {
-        request(api).get('/').expect(200);
+    it("Responds to GET request at  with a 200 status", (done) => {
+        request(api).get('/').expect(200, done);
     })
+
+    it("Responds to GET request at /flavours with a 200 status", (done) => {
+        request(api).get('/flavours').expect(200, done);
+    })
+
+    it("Responds to GET request at /flavours with a JSON object", (done) => {
+        request(api).get('/flavours').expect('Content-Type', /json/, done);
+    })
+
+    // it("Responds to GET request at /flavours with a JSON object that has a flavours key", (done) => {
+    //     request(api).get('/flavours').end((err, res) => {
+    //         const data = res.body;
+    //         expect('flavours' in data)
+    //         expect(Array.isArray(data['flavours']));
+    //         done();
+    //     });
+    // })
+
+    
 
 })
